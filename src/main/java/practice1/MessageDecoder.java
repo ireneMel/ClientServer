252810b5b2f25d9868ba8ee2d5beb921f88b242a
ireneMiller <iryna2.melnyk@ukma.edu.com>
2022-06-11 21:49:15 +0300

    Added Message encoding/decoding

---
+++ src/main/java/practice1/MessageDecoder.java
@@ -0,0 +1,38 @@
+package practice1;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import java.nio.ByteBuffer;
+
+public class MessageDecoder {
+    private final byte[] wholeMessage;
+    private final long seed;
+
+    public MessageDecoder(byte[] wholeMessage, long seed) {
+        this.wholeMessage = wholeMessage;
+        this.seed = seed;
+    }
+
+    public Message getMessage() {
+        ByteBuffer buffer = ByteBuffer.wrap(decode(wholeMessage));
+
+        int cType = buffer.getInt();
+        int userId = buffer.getInt();
+        byte[] messageBody = new byte[buffer.array().length - 8];
+        buffer.get(messageBody, 0, buffer.array().length - 8);
+        return new Message(cType, userId, messageBody);
+    }
+
+    private byte[] decode(byte[] message) {
+        try {
+            Cipher c = Cipher.getInstance("AES");
+            SecretKey key = Message.generateKey(seed);
+            c.init(Cipher.DECRYPT_MODE, Message.generateKey(seed));
+            return c.doFinal(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
---
+++ src/main/java/practice1/MessageEncoder.java
@@ -0,0 +1,38 @@
+package practice1;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
+
+public class MessageEncoder {
+    private final Message message;
+    private final long seed;
+
+    public MessageEncoder(Message message, long seed) {
+        this.message = message;
+        this.seed = seed;
+    }
+
+    public byte[] getBytes() {
+        ByteBuffer buffer = ByteBuffer.allocate(message.getMessageBody().length + 8)
+                .putInt(message.getCType())
+                .putInt(message.getUserId())
+                .put(message.getMessageBody());
+        return encode(buffer.array());
+    }
+
+    private byte[] encode(byte[] message) {
+        try {
+            Cipher c = Cipher.getInstance("AES");
+            SecretKey key = Message.generateKey(seed);
+//            SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
+            c.init(Cipher.ENCRYPT_MODE, Message.generateKey(seed));
+            return c.doFinal(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
