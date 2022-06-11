34d4784ee41cbf83ed9207313332657ce9c2d310
ireneMiller <iryna2.melnyk@ukma.edu.com>
2022-06-11 21:47:04 +0300

    Added POJOs

---
+++ src/main/java/practice1/Package.java
@@ -0,0 +1,23 @@
+package practice1;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+public class Package {
+    public static final byte bMagic = 0x13;
+    public static final int HEADER_LENGTH = 14;
+
+    private final byte bSrc; //Унікальний номер клієнтського застосування
+    private final long packetId; //Номер повідомлення
+    private final int wLen;
+    private final Message message;
+
+    public Package(byte bSrc, long packetId, int wLen, Message message) {
+        this.bSrc = bSrc;
+        this.packetId = packetId;
+        this.message = message;
+        this.wLen = wLen;
+    }
+}
