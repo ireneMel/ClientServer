c4a4b8cef6b490c66accb61f2834b739aa92acf9
zloydi <dobrovolskyivan2002@gmail.com>
2022-06-12 08:41:18 +0300

    Added tests

---
+++ src/test/java/practice1/PackageTest.java
@@ -0,0 +1,41 @@
+package practice1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+//тести мають бути без модифікаторів доступу
+class PackageTest {
+
+    @Test
+    void shouldHandlePackage() throws Exception {
+        byte[] test = new byte[]{1, 2, 3, 4, 5};
+        Message message = new Message(1, 1, test);
+
+        Package aPackage = new Package((byte) 1, 1, 16, message);
+
+        PackageEncoder packageEncoder = new PackageEncoder(aPackage);
+        byte[] pac = packageEncoder.getBytes();
+
+        PackageDecoder packageDecoder = new PackageDecoder(pac);
+
+        assertEquals(packageDecoder.getPackage(), aPackage);
+    }
+
+    @Test
+    void shouldHandleInvalidCrc_header() {
+        byte[] test = new byte[]{1, 2, 3, 4, 5};
+        Message message = new Message(1, 1, test);
+
+        Package aPackage = new Package((byte) 1, 1, 16, message);
+        PackageEncoder packageEncoder = new PackageEncoder(aPackage);
+        byte[] pac = packageEncoder.getBytes();
+
+        pac[1] = 0x1;
+        pac[10] = 0x1;
+
+        PackageDecoder packageDecoder = new PackageDecoder(pac);
+        assertThrows(Exception.class, packageDecoder::getPackage);
+    }
+}
