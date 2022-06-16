51baac2b86e5f02d696b11ee2c0219117bb18d3e
zloydi <dobrovolskyivan2002@gmail.com>
2022-06-16 19:56:11 +0300

    Added Commands

---
+++ src/main/java/homework/utils/Commands.java
@@ -0,0 +1,23 @@
+package homework.utils;
+
+/*
+    Взнати кількість товару на складі
+    Списати певну кількість товару
+    Зарахувати певну кількість товару
+    Додати групу товарів
+    Додати назву товару до групи
+    Встановити ціну на конкретний товар
+ */
+
+public class Commands {
+    static final int
+            PRODUCT_GET = 0,
+            PRODUCT_ADD_VALUE = 1,
+            PRODUCT_REMOVE_VALUE = 2,
+            GROUP_CREATE = 3,
+            PRODUCT_ADD_TO_GROUP = 4,
+            PRODUCT_SET_PRICE = 5;
+
+}
+
+
